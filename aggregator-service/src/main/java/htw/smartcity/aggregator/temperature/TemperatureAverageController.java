e6c8f65a89519039bf79f947053273c63ba7629b
david <kiefer97@web.de>
2020-07-23 17:51:04 +0200

    Temperature Average Sachen

---
+++ aggregator-service/src/main/java/htw/smartcity/aggregator/temperature/TemperatureAverageController.java
@@ -0,0 +1,5 @@
+package htw.smartcity.aggregator.temperature;
+
+public class TemperatureAverageController
+{
+}
---
+++ aggregator-service/src/main/java/htw/smartcity/aggregator/temperature/TemperatureAverageMonthly.java
@@ -0,0 +1,123 @@
+package htw.smartcity.aggregator.temperature;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import htw.smartcity.aggregator.sensor.Sensor;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+@Table (name = "TemperatureAverageMonthly")
+@JsonIgnoreProperties ({"sensor"})
+public class TemperatureAverageMonthly
+{
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue (strategy= GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn (name = "sensor_id")
+    private Sensor sensor;
+
+
+    @Column(name ="begin_time")
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date beginTime;
+
+
+    @Column(name ="end_time")
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date endTime;
+
+    @Column(name = "value")
+    private Double value;
+
+
+    /**
+     * Default constructor for JPA only.
+     */
+    public TemperatureAverageMonthly() {
+
+    }
+
+    // Average of 1 sensor in a timeframe
+    public TemperatureAverageMonthly(Sensor sensor, Date beginTime, Date endTime, Double value) {
+        this.sensor = sensor;
+        this.beginTime = beginTime;
+        this.endTime = endTime;
+        this.value = value;
+    }
+
+    // Average of all sensors in a timeframe
+    public TemperatureAverageMonthly(Date beginTime, Date endTime, Double value) {
+        this.beginTime = beginTime;
+        this.endTime = endTime;
+        this.value = value;
+    }
+
+    // Average of all sensors all time
+    public TemperatureAverageMonthly(Double value) {
+        this.value = value;
+    }
+
+    // Average of 1 sensor all time
+    public TemperatureAverageMonthly(Sensor sensor, Double value) {
+        this.sensor = sensor;
+        this.value = value;
+    }
+
+
+
+    public Long getId() {
+        return id;
+    }
+
+    /**
+     * Set JPA id - for testing and JPA only. Not intended for normal use.
+     *
+     * @param id
+     *            The new id.
+     */
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Sensor getSensor() {
+        return sensor;
+    }
+
+    public void setSensor(Sensor sensor) {
+        this.sensor = sensor;
+    }
+
+    public Date getBeginTime() {
+        return beginTime;
+    }
+
+    public void setBeginTime(Date beginTime) {
+        this.beginTime = beginTime;
+    }
+
+    public Date getEndTime() {
+        return endTime;
+    }
+
+    public void setEndTime(Date endTime) {
+        this.endTime = endTime;
+    }
+
+    public Double getValue() {
+        return value;
+    }
+
+    public void setValue(Double value) {
+        this.value = value;
+    }
+
+    @Override
+    public String toString() {
+        return sensor.toString() + beginTime.toString() + "-" + endTime.toString() + ":" + value.toString();
+    }
+}
