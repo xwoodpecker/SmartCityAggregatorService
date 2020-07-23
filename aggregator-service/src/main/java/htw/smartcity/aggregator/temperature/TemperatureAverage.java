b42aa80874396aa353dbdc8bbc9f17d0aaa7a021
Maverick Studer <maverick.studer95@gmail.com>
2020-07-23 18:00:52 +0200

    fixed mappedsupperclass for david my friend and helper <3

---
+++ aggregator-service/src/main/java/htw/smartcity/aggregator/temperature/TemperatureAverage.java
@@ -0,0 +1,69 @@
+package htw.smartcity.aggregator.temperature;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import htw.smartcity.aggregator.sensor.Sensor;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@MappedSuperclass
+@JsonIgnoreProperties({"sensor"})
+public class TemperatureAverage {
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "sensor_id")
+    private Sensor sensor;
+
+
+    @Column(name = "value")
+    private Double value;
+
+
+    /**
+     * Default constructor for JPA only.
+     */
+    public TemperatureAverage() {
+
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    /**
+     * Set JPA id - for testing and JPA only. Not intended for normal use.
+     *
+     * @param id
+     *            The new id.
+     */
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Sensor getSensor() {
+        return sensor;
+    }
+
+    public void setSensor(Sensor sensor) {
+        this.sensor = sensor;
+    }
+
+    public Double getValue() {
+        return value;
+    }
+
+    public void setValue(Double value) {
+        this.value = value;
+    }
+
+
+    @Override
+    public String toString() {
+        return "[" + value + "]";
+    }
+}
