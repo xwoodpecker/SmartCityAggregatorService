0eecf4eddf6b320b99dacbd672837e72b3ec4d57
NewYearNewPhil <pallweyer@htwsaar.de>
2020-08-30 12:11:40 +0200

    changed date serialization format to be compatible with OffsetDateTime

--- aggregator-service/src/main/java/htw/smartcity/aggregator/parking/ParkingGroupCounter.java
+++ aggregator-service/src/main/java/htw/smartcity/aggregator/parking/ParkingGroupCounter.java
@@ -14,7 +14,7 @@ public class ParkingGroupCounter {
     @GeneratedValue(strategy=GenerationType.IDENTITY)
     private Long id;
 
-    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'")
     @Column(name = "time")
     private LocalDateTime time;
 
