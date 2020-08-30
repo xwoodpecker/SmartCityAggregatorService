3b227f3f6f2262f76f5df24fa8442b1ab9f6d7c9
NewYearNewPhil <pallweyer@htwsaar.de>
2020-08-30 15:38:12 +0200

    added hibernate entities for users, authorities

---
+++ aggregator-service/src/main/java/htw/smartcity/aggregator/security/WebSecurityConfig.java
@@ -0,0 +1,59 @@
+package htw.smartcity.aggregator.security;
+
+import htw.smartcity.aggregator.util.ConfigProperties;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import javax.sql.DataSource;
+
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter
+{
+    private final String REST_ADMIN_PASSWORD = "Ebp4Uw5UfajpHJvv";
+
+    @Autowired
+    private UserDetailsService userDetailsService;
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Autowired
+    public void configure(AuthenticationManagerBuilder auth)
+            throws Exception
+    {
+        auth.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder());
+        /*auth.jdbcAuthentication()
+                .dataSource(dataSource)
+                .withDefaultSchema()
+                .withUser("user")
+                    .password(passwordEncoder.encode(ConfigProperties.REST_USER_PASSWORD))
+                    .roles("USER")
+                .and()
+                .withUser("admin")
+                    .password(passwordEncoder.encode(REST_ADMIN_PASSWORD))
+                    .roles("USER", "ADMIN");
+                    
+         */
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception
+    {
+        //http.csrf().disable().authorizeRequests().anyRequest().authenticated().and().httpBasic();
+        http.authorizeRequests().anyRequest().hasAnyRole("ADMIN", "USER")
+                .and()
+                .authorizeRequests().antMatchers("/temperatures**").permitAll()
+                .and().csrf().disable();
+    }
+}
