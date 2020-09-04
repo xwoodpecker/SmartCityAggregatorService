fdb45ff2649054778dd7a934859b4256c7ec0ac5
david <kiefer97@web.de>
2020-09-04 17:06:10 +0200

    TemperatureAggregatorComputationSchedulerTests

---
+++ aggregator-service/src/test/java/htw/smartcity/aggregator/temperature/TemperatureAggregateComputationSchedulerTests.java
@@ -0,0 +1,62 @@
+package htw.smartcity.aggregator.temperature;
+
+import htw.smartcity.aggregator.sensor.Sensor;
+import htw.smartcity.aggregator.sensor.SensorRepository;
+import htw.smartcity.aggregator.sensor.SensorType;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.jdbc.EmbeddedDatabaseConnection;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.Month;
+import java.util.List;
+
+@RunWith (SpringRunner.class)
+@DataJpaTest
+@AutoConfigureTestDatabase (connection = EmbeddedDatabaseConnection.H2)
+public class TemperatureAggregateComputationSchedulerTests
+{
+    @Autowired
+    SensorRepository sensorRepository;
+
+    @Autowired
+    TemperatureRepository temperatureRepository;
+
+    @Autowired
+    TemperatureAggregateRepository temperatureAggregateRepository;
+
+    @Test
+    public void dailyAggregation()
+    {
+        TemperatureAggregateComputationScheduler scheduler = new TemperatureAggregateComputationScheduler();
+
+        scheduler.computeAggregatesDaily((long) 8, LocalDateTime.of(2020, Month.SEPTEMBER, 1, 0, 0, 0),
+                                         LocalDateTime.of(2020, Month.SEPTEMBER, 1, 23, 59, 59));
+
+    }
+
+    @Test
+    public void weeklyAggregation()
+    {
+        TemperatureAggregateComputationScheduler scheduler = new TemperatureAggregateComputationScheduler();
+
+        scheduler.computeAggregatesWeekly((long) 8, LocalDateTime.of(2020, Month.AUGUST, 31, 0, 0, 0),
+                                         LocalDateTime.of(2020, Month.SEPTEMBER, 6, 23, 59, 59));
+
+    }
+
+    @Test
+    public void monthlyAggregation()
+    {
+        TemperatureAggregateComputationScheduler scheduler = new TemperatureAggregateComputationScheduler();
+
+        scheduler.computeAggregatesMonthly((long) 8, LocalDateTime.of(2020, Month.SEPTEMBER, 1, 0, 0, 0),
+                                          LocalDateTime.of(2020, Month.OCTOBER, 1, 23, 59, 59));
+
+    }
+}
